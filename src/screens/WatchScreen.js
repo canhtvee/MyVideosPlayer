9d5d84be38fe98ef7370f30523eb5f6a591bfa8c
Trinh Van Canh <canhtv@rabiloo.com>
2022-01-21 13:40:36 +0700

    webview updated

--- src/screens/WatchScreen.js
+++ src/screens/WatchScreen.js
@@ -3,12 +3,6 @@ import {View, Text} from 'react-native';
 import WebView from 'react-native-webview';
 
 export default function WatchScreen() {
-  const runFirst = () => {
-    if (global.window) {
-      global.window = global;
-      setTimeout(() => window.ReactNativeWebView.postMessage('Hello!'), 2000);
-    }
-  };
   return (
     <View style={{flex: 1}}>
       <Text style={{height: 40, textAlign: 'center', fontSize: 24}}>
